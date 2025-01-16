05ecf92c6f55e619324d9d7491bac8999a6747b9
Nikola Stojanović <153657652+Stojanovic94@users.noreply.github.com>
2025-01-17 00:05:01 +0100

    Update SingletonDemo.java

--- src/singleton/SingletonDemo.java
+++ src/singleton/SingletonDemo.java
@@ -2,10 +2,12 @@ package singleton;
 
 public class SingletonDemo {
 	public static void main(String[] args) {
+		
 		// Dobijanje instance klase DatabaseConnection
 		DatabaseConnection connection1 = DatabaseConnection.getInstance();
 		DatabaseConnection connection2 = DatabaseConnection.getInstance();
-		// Provera da li su obe instance iste
+		
+		// Proverava da li su obe instance iste
 		if (connection1 == connection2) {
 			System.out.println("Obe instance su identične.");
 			} else {
